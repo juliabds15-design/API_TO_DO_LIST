17944c2dbeb998070a9c64d92e991e3494f2d6b0
Júlia Bonini <juliabds15@gmail.com>
2026-05-28 18:02:51 -0300

    Atualizações 2° Bimestre

--- src/routes/taskroutes.js
+++ src/routes/taskroutes.js
@@ -5,29 +5,29 @@ module.exports = (req, res) => {
   const url = req.url;
   const method = req.method;
 
-  // GET /tasks
+  //GET /tasks
   if (url === '/tasks' && method === 'GET') {
     return taskController.listTasks(req, res);
   }
 
-  // POST /tasks
+  //POST /tasks
   if (url === '/tasks' && method === 'POST') {
     return taskController.createTask(req, res);
   }
 
-  // PUT /tasks/:id
+  //PUT /tasks/:id
   if (url.startsWith('/tasks/') && method === 'PUT') {
     const id = url.split('/')[2];
     return taskController.updateTask(req, res, id);
   }
 
-  // DELETE /tasks/:id
+  //DELETE /tasks/:id
   if (url.startsWith('/tasks/') && method === 'DELETE') {
     const id = url.split('/')[2];
     return taskController.deleteTask(req, res, id);
   }
 
-  // Rota não encontrada
+  //Rota não encontrada
   res.statusCode = 404;
   res.end(JSON.stringify({ message: 'Rota não encontrada' }));
 };
